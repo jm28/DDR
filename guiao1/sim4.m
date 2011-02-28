af5812985c6edfda19d567d36f9b1525c9efe2b7
Rafael <rafapinto18@hotmail.com>
2011-02-28 18:37:51 +0000

    final version of the simulator 2 & simulator 4

---
+++ guiao1/sim4.m
@@ -0,0 +1,128 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Função para cálculo da média e do intervalo de confiança   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function[mediabloqueio, iconfbloq, mediapiorbloqueio, iconfpiorbloq, mediaocupacao, iconfocup] = sim4(l, dm, c, n, p, ncorrida)
+
+sbloqueio = [];
+socupacao = [];
+spiorbloqueio = [];
+i = 0;
+
+while i < ncorrida
+    [bloqueio bloqueiopior mediaOcupacao] = simulador4(l, dm, c, n, p);
+    sbloqueio = [sbloqueio bloqueio];
+    socupacao = [socupacao mediaOcupacao];
+    spiorbloqueio = [spiorbloqueio bloqueiopior];
+    i = i +1;
+end
+
+mediabloqueio = mean(sbloqueio);
+mediaocupacao = mean(socupacao);
+mediapiorbloqueio = mean(spiorbloqueio);
+
+variancebloqueio = var(sbloqueio);
+varianceocupacao = var(socupacao);
+variancepiorbloqueio = var(spiorbloqueio);
+
+iconfbloq = norminv(0.95) * sqrt(variancebloqueio/ncorrida);
+iconfocup = norminv(0.95) * sqrt(varianceocupacao/ncorrida);
+iconfpiorbloq = norminv(0.95) * sqrt(variancepiorbloqueio/ncorrida);
+
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function[bloqueio, bloqueiopior, mediaOcupacao] = simulador4(l, dm, c, n, p)
+bloqueadas = 0;
+eventos = [];
+chamadasporcanal = zeros(n);
+estados = zeros(n);
+ocupacao = 0;
+nchamadas = 0;
+
+for i = 1:n-1
+    for j = i+1:n
+        eventos = [eventos; exprnd(l(i,j)) 0 i j]; %% evento de chegada
+    end
+end
+
+tempoUltevento = 0;
+
+while nchamadas < p,
+    eventos = sortrows(eventos);
+    pnos = eventos(1,3:4); % par dos nós em que o eventos vai ser processado
+    tempochamadaproc = eventos(1,1);
+    
+    if eventos(1,2) == 1 % é uma partida
+        eventos(1,:) = []; % retirar evento
+        ocupacao = ocupacao + (tempochamadaproc-tempoUltevento)*sum(sum(triu(estados)));
+        estados(pnos(1), pnos(2)) = estados(pnos(1), pnos(2)) - 1;
+    else
+        eventos(1,:) = []; % retirar evento
+        ocupacao = ocupacao + (tempochamadaproc-tempoUltevento)*sum(sum(triu(estados)));
+        nchamadas = nchamadas + 1;
+        chamadasporcanal(pnos(1),pnos(2)) = chamadasporcanal(pnos(1), pnos(2)) + 1;
+        
+        if estados(pnos(1), pnos(2)) < c %% acesso directo
+            estados(pnos(1), pnos(2)) = estados(pnos(1), pnos(2)) + 1;
+            eventos = [eventos; (tempochamadaproc + exprnd(dm)) 1 pnos]; %%agendar partida
+            
+        else
+            caminho = melhorcaminho(estados,c,pnos, 1:n); % calcular o melhor caminho possível
+            
+            if caminho ==  -1
+                estados(pnos(2), pnos(1)) = estados(pnos(2), pnos(1)) + 1;
+            else
+                orint = sort([pnos(1), caminho]); % canal origem -> intermedio
+                intdest = sort([pnos(2), caminho]); % canal intermedio -> destino
+                estados(orint(1), orint(2)) = estados(orint(1), orint(2)) + 1;
+                estados(intdest(1), intdest(2)) = estados(intdest(1), intdest(2)) + 1;
+                tempo = (tempochamadaproc + exprnd(dm)); %% tempo de agendamento das partidas
+                eventos = [eventos; tempo 1 orint; tempo 1 intdest]; %%agendar partida
+            end
+        end
+        eventos = [eventos; (tempochamadaproc + exprnd(l(pnos(1),pnos(2)))) 0 pnos]; % agendar chegada para o par de nós processado
+        tempoUltevento = tempochamadaproc;
+    end
+    
+end
+    bloqueio = sum(sum(tril(estados)))/p;
+    bloqueadasporcanal = triu(estados);
+    [i,j] = find(bloqueadasporcanal==max(bloqueadasporcanal(:)));
+    bloqueiopior = max(bloqueadasporcanal(:))/chamadasporcanal(i,j);  
+    mediaOcupacao = (ocupacao/tempochamadaproc)/(n*(n-1)/2);
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function caminho  = melhorcaminho(estado, c, pnos, totalnos)
+caminho = -1;
+intermedios = setdiff(totalnos, pnos); % retirar par origem e destino da lista de nos
+cargas = [];
+
+
+if size(intermedios) > 0
+    for i = 1:size(intermedios)
+        orint = sort([pnos(1), intermedios(i)]);
+        intdest = sort([pnos(2), intermedios(i)]);
+        e1 = classificarLigacao(c, estado(orint(1), orint(2)));
+        e2 = classificarLigacao(c, estado(intdest(1), intdest(2)));
+        cargas = [cargas; intermedios(i) max(e1,e2)];
+    end
+    
+    if size(cargas)> 0
+        cargas = sortrows(cargas, 2);
+        if cargas(1) < c
+            caminho = cargas(1);
+        end
+    end
+end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function classificacao = classificarLigacao(c, ncircuitos)
+
+if ncircuitos <= c/2
+    classificacao = 0; % CR
+elseif ncircuitos > c/2 && ncircuitos < c
+    classificacao = 1; % CE
+else
+    classificacao = 2; % OC
+end
+end
--- guiao1/simulador2.m
+++ guiao1/simulador2.m
@@ -1,60 +1,86 @@
-
-function[bloqueio1, iconfbloq1, mediaOcupacao1, iconfocup1,bloqueio2, iconfbloq2,mediaOcupacao2, iconfocup2,bloqueio, iconfbloq, mediaOcupacao, iconfocup] = simulador2(l, dm, c, p)
-
-chamadas = [];
-sbloqueio = [];
-socupacao = [];
-nchamadas = 0;
-l = l/60;
-
-while nchamadas < p,
-    [chamadas, bloqueadas, ocupacao] = simular(l, dm, c, chamadas);
-    
-    if nchamadas == 20
-        [bloqueio1, iconfbloq1, mediaOcupacao1, iconfocup1] = intervalo(sbloqueio, socupacao, nchamadas);
-    elseif (nchamadas == 201)
-        while nchamadas < 220
-            sbloqueio = [sbloqueio bloqueadas];
-            socupacao = [socupacao ocupacao];
-            nchamadas = nchamadas + 1;
-        end
-        [bloqueio2, iconfbloq2, mediaOcupacao2, iconfocup2] = intervalo(sbloqueio, socupacao, nchamadas);
-    else
-        sbloqueio = [sbloqueio bloqueadas];
-        socupacao = [socupacao ocupacao];
-    end
-    nchamadas = nchamadas + 1;
-end
-
-[bloqueio, iconfbloq, mediaOcupacao, iconfocup] = intervalo(sbloqueio, socupacao,nchamadas);
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Função para cálculo da média e do intervalo de confiança   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function[mediabloqueio, iconfbloq, mediaocupacao, iconfocup] = intervalo(sbloqueio, socupacao, nchamadas)
+function[mediabloqueio1, iconfbloq1, mediaocupacao1, iconfocup1, mediabloqueio2, iconfbloq2, mediaocupacao2, iconfocup2] = simulador2(l, dm, c, p, ncorrida)
 
-mediabloqueio = mean(sbloqueio);
-mediaocupacao = mean(socupacao);
+sbloqueio1 = [];
+socupacao1 = [];
+sbloqueio2 = [];
+socupacao2 = [];
+i = 0;
 
-variancebloqueio = var(sbloqueio);
-varianceocupacao = var(socupacao);
-
-iconfbloq = norminv(0.95) * sqrt(variancebloqueio/nchamadas);
-iconfocup = norminv(0.95) * sqrt(varianceocupacao/nchamadas);
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function[chamadas, bloqueadas, ocupacao] = simular(l, dm, c, chamadas)
-
-tchamada = exprnd(1/l); %% evento de chegada
-
-chamadas = chamadas - tchamada; %% retirar o tempo que passou
-chamadas = chamadas(chamadas>0); %% retirar chamadas terminadas
-ocupacao = size(chamadas, 2); %% actualizar ocupacao
-
-if size(chamadas, 2) < c
-    chamadas = [chamadas exprnd(dm)];
-    bloqueadas = 0;
-else
-    bloqueadas =  1;
+while i < ncorrida
+    [bloqueio1 mediaOcupacao1 bloqueio2 mediaOcupacao2] = sim2(l, dm, c, p);
+    sbloqueio1 = [sbloqueio1 bloqueio1];
+    socupacao1 = [socupacao1 mediaOcupacao1];
+    sbloqueio2 = [sbloqueio2 bloqueio2];
+    socupacao2 = [socupacao2 mediaOcupacao2];
+    i = i +1;
 end
+
+mediabloqueio1 = mean(sbloqueio1);
+mediaocupacao1 = mean(socupacao1);
+
+mediabloqueio2 = mean(sbloqueio2);
+mediaocupacao2 = mean(socupacao2);
+
+variancebloqueio1 = var(sbloqueio1);
+varianceocupacao1 = var(socupacao1);
+
+variancebloqueio2 = var(sbloqueio2);
+varianceocupacao2 = var(socupacao2);
+
+iconfbloq1 = norminv(0.95) * sqrt(variancebloqueio1/ncorrida);
+iconfocup1 = norminv(0.95) * sqrt(varianceocupacao1/ncorrida);
+
+iconfbloq2 = norminv(0.95) * sqrt(variancebloqueio2/ncorrida);
+iconfocup2 = norminv(0.95) * sqrt(varianceocupacao2/ncorrida);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function[bloqueio1, mediaOcupacao1, bloqueio2, mediaOcupacao2] = sim2(l, dm, c, p)
+bloqueadas = 0;
+estado = 0;
+ocupacao = 0;
+nchamadas = 0;
+l = 60/l;
+
+eventos = [exprnd(l) 0]; %% agendar evento de chegada
+ultimoevento = 0;
+
+while nchamadas < p,
+           
+        eventos = sortrows(eventos);
+        eventoproc = eventos(1,1); % evento que vai ser processado
+        
+        if eventos(1,2) == 1 % é uma partida
+            eventos(1,:) = []; % retirar evento
+            ocupacao = ocupacao + (eventoproc-ultimoevento)*estado; %actualizar ocupação
+            estado = estado - 1; %decrementar estado
+        else
+            eventos(1,:) = []; % retirar evento, neste caso uma chegada
+            ocupacao = ocupacao + (eventoproc-ultimoevento)*estado; %actualizar ocupação
+            nchamadas = nchamadas + 1; %incrementar número de chegadas
+            
+            if estado < c % Ainda existem circuitos livres ?
+                estado = estado + 1; % incrementar o estado, pois vai ser ocupado um circuito
+                eventos = [eventos; (eventoproc + exprnd(dm)) 1]; %agendar um evento de partida
+            else
+                bloqueadas = bloqueadas + 1; %Não há circuitos livres, logo a chegada é bloqueada
+            end
+            eventos = [eventos; (eventoproc + exprnd(l)) 0]; %agendar um evento de chegada
+        end
+        
+        ultimoevento = eventoproc; %guardar o último evento processado
+        
+    if nchamadas == 20
+        bloqueio1 = bloqueadas/nchamadas; %calcular probabilidade de bloqueio
+        mediaOcupacao1 = ocupacao/ultimoevento; %calcular ocupação média
+    elseif nchamadas == 200
+        bloqueadas = 0;
+        ocupacao = 0;
+    elseif nchamadas == 220
+        bloqueio2 = bloqueadas/nchamadas; %calcular probabilidade de bloqueio
+        mediaOcupacao2 = ocupacao/ultimoevento; %calcular ocupação média
+    end
+end
+
