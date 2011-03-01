513629e3b51caf439950c888b0f1de0f87c362e7
Rafael <rafapinto18@hotmail.com>
2011-03-01 11:17:35 +0000

    Refactoring of the simulator

--- guiao1/simulador1_ver2.m
+++ guiao1/simulador1_ver2.m
@@ -26,40 +26,40 @@ iconfocup = norminv(0.95) * sqrt(varianceocupacao/ncorrida);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function[bloqueio, mediaOcupacao] = simulador1(l, dm, c, p)
 bloqueadas = 0;
-estado = 0;
+estadoLigacoes = 0;
 ocupacao = 0;
 nchamadas = 0;
-l = l/60;
+l = 60/l;
 
-chamadas = [exprnd(1/l) 0]; %% evento de chegada
-ultimoevento = 0;
+eventos = [exprnd(l) 0]; %% evento de chegada
+tempoultimoevento = 0;
 
 while nchamadas < p,
     
-   chamadas = sortrows(chamadas);
+   eventos = sortrows(eventos); 
    
-   chamadaproc = chamadas(1,1);
+   tempoEventoproc = eventos(1,1); %tempo do evento que vai ser processado
    
-   if chamadas(1,2) == 1 % é uma partida 
-       chamadas(1,:) = []; % retirar evento
-       ocupacao = ocupacao + (chamadaproc-ultimoevento)*estado;
-       estado = estado - 1;
+   if eventos(1,2) == 1 % é uma partida 
+       eventos(1,:) = []; % retirar evento
+       ocupacao = ocupacao + (tempoEventoproc-tempoultimoevento)*estadoLigacoes; %actualiza o tempo das ocupacoes
+       estadoLigacoes = estadoLigacoes - 1; %liberta um canal
    else
-       chamadas(1,:) = []; % retirar evento
-       ocupacao = ocupacao + (chamadaproc-ultimoevento)*estado;
-       nchamadas = nchamadas + 1;
+       eventos(1,:) = []; % retirar evento
+       ocupacao = ocupacao + (tempoEventoproc-tempoultimoevento)*estadoLigacoes; %actualizar o tempo da ocupação
+       nchamadas = nchamadas + 1; % incrementa o número de chamadas recebidas
                                   
-        if estado < c
-            estado = estado + 1;
-            chamadas = [chamadas; (chamadaproc + exprnd(dm)) 1];
+        if estadoLigacoes < c %ainda existem canais livres ?
+            estadoLigacoes = estadoLigacoes + 1; % ocupa um canal
+            eventos = [eventos; (tempoEventoproc + exprnd(dm)) 1]; %agenda um evento de partida
         else
-            bloqueadas = bloqueadas + 1;
+            bloqueadas = bloqueadas + 1; %incrementa o número de chamadas bloqueadas
         end
-        chamadas = [chamadas; (chamadaproc + exprnd(1/l)) 0];
+        eventos = [eventos; (tempoEventoproc + exprnd(l)) 0]; %agenda um evento de chegada
    end
     
-    ultimoevento = chamadaproc;
+    tempoultimoevento = tempoEventoproc;
 end
 
 bloqueio = bloqueadas/p;
-mediaOcupacao = ocupacao/chamadaproc;
+mediaOcupacao = ocupacao/tempoEventoproc;
